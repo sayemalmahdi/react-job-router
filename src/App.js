258925cfe5c71c35b06c38f2e4311ea7061bf4db
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-01-11 21:56:48 +0600

    React Router Basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
   return (
-    <div className="App">
-      Hello, There!
-    </div>
+    <BrowserRouter>
+    <header>
+        <nav>
+          <h1>React-Job-Router</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+    <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
 export default App;
+  
