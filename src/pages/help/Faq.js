f849fe4cc7c50c37a5e55ed6491c9d9925976a47
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-01-17 14:21:27 +0600

    Remove some Data from Faq.js And db.json (For Avoiding Scrolling)

--- src/pages/help/Faq.js
+++ src/pages/help/Faq.js
@@ -12,22 +12,7 @@ export default function Faq() {
           <p><strong>Lorem ipsum dolor sit amet.</strong></p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
         </div>
-  
-        <div className="question">
-          <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-        </div>
-  
-        <div className="question">
-          <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-        </div>
-  
-        <div className="question">
-          <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-        </div>
-  
+        
       </div>
     )
   }
